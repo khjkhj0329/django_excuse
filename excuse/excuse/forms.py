bc7a2caad1d03a8562d53f598fd514d7f9b06d92
효정이 <w2029@e-mirim.hs.kr>
2022-07-08 14:24:03 +0900

    텍스트 정렬

--- excuse/excuse/forms.py
+++ excuse/excuse/forms.py
@@ -4,8 +4,8 @@ from .models import Excuse, Board
 
 class QuestionForm(forms.ModelForm):
     class Meta:
-        model = Excuse  # 사용할 모델
-        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
+        model = Excuse
+        fields = ['subject', 'content']
         widgets = {
             'subject': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
